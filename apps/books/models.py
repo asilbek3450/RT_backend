38de33e489dc318302e9ce03019d9e84fc22a7a5
Asilbek Mirolimov <asilbekmirolimov@gmail.com>
2022-11-12 17:39:54 +0500

    change

--- apps/books/models.py
+++ apps/books/models.py
@@ -26,11 +26,7 @@ class BookType(models.Model):
 class Book(models.Model):
     science1 = models.ForeignKey(Science, on_delete=models.CASCADE, null=False, blank=False, related_name='science1')
     science2 = models.ForeignKey(Science, on_delete=models.CASCADE, null=False, blank=False, related_name='science2')
-<<<<<<< HEAD
     language_id = models.ForeignKey("users.LanguageChoices", on_delete=models.CASCADE, null=False, blank=False)
-=======
-    language_id = models.ForeignKey("users.LanguageChoices", on_delete=models.CASCADE, null=True, blank=True)
->>>>>>> 3702e74f09e38eacde3501e99e2aab301d651d8e
     book_type = models.ForeignKey("books.BookType", on_delete=models.CASCADE, null=True, blank=True)
     is_free = models.BooleanField(default=True)
 
@@ -38,8 +34,4 @@ class Book(models.Model):
         ordering = ['id']
 
     def __str__(self):
-<<<<<<< HEAD
         return f'{self.id} {self.science1.title} - {self.science2.title}'
-=======
-        return f'{str(self.id)} {str(self.science1)} - {str(self.science2)}'
->>>>>>> 3702e74f09e38eacde3501e99e2aab301d651d8e
