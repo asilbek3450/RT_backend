33cbf808d42023ea498d45ebc9787868abeafc26
asilbek <asilbekmirolimov@gmail.com>
2022-11-22 13:58:42 +0500

    changes

--- apps/books/serializers.py
+++ apps/books/serializers.py
@@ -15,7 +15,7 @@ class ScienceSerializer(ModelSerializer):
 class BookTypeSerializer(ModelSerializer):
     class Meta:
         model = BookType
-        fields = ('id', 'title')
+        fields = ['id', 'title']
 
         read_only_fields = ['id']
 
@@ -24,17 +24,18 @@ class BookSerializer(ModelSerializer):
 
     class Meta:
         model = Book
-        fields = ('id', 'science1', 'science2', 'language_id', 'book_type', 'is_free')
+        fields = [
+            'id',
+            'science1',
+            'science2',
+            'language_id',
+            'book_type',
+            'is_free',
+        ]
 
         read_only_fields = ['id']
 
         depth = 1
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['science1'] = instance.science1.title
-        representation['science2'] = instance.science2.title
-        return representation
 
 
 class BookCreateUpdateSerializer(ModelSerializer):
